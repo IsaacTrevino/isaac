1c95b359d19d2b953f196ab5d9ae348f62bf4702
Isaac Trevino <isaac.trevino@outlook.com>
2024-04-06 16:51:36 -0500

    react app

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "isaac": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
